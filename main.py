3976acde730404edbed58281f87b4b4d306ebf87
kulvirvirk <kvirkdev@gmail.com>
2020-04-18 19:53:32 +0000

    print both dictionaries

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # 1. make two dictionaries with four items
+# 2. print both dictionaries; hint - convert 'dict' to 'str' type
 
 
 
@@ -17,6 +18,9 @@ friend2 = {
   'city': 'Corona'}
 
 
-
+# 2. print both dictionaries; hint - convert 'dict' to 'str' type
+print('My friend 1 details:' + str(friend1))
+print('My friend 2 details:' + str(friend2))
+print('===============**********===============')
 
 
