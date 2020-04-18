76a938954fb5132adb36a1d9ef51e82cc52f466f
kulvirvirk <kvirkdev@gmail.com>
2020-04-18 19:47:08 +0000

    Created two dictionaries friend1 and friend2

---
+++ main.py
@@ -0,0 +1,22 @@
+# 1. make two dictionaries with four items
+
+
+
+
+# 1. make two dictionaries with four items 
+friend1 = {
+  'name': 'Gunu',
+  'height': 3,
+  'age': 5, 
+  'city': 'Corona'}
+
+friend2 = {
+  'name': 'Simrat',
+  'height': 4,
+  'age': 7, 
+  'city': 'Corona'}
+
+
+
+
+
